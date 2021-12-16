8cc84937a240a2b12b0eb58cd73f4fad3f0bb49b
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-16 13:44:57 +0530

    Refactor to reuse controls on button

--- picture-in-picture/scripts/index.js
+++ picture-in-picture/scripts/index.js
@@ -5,23 +5,67 @@ const cancelPipBtn = document.querySelector('#cancelPipBtn');
 
 const video = document.querySelector('#video');
 
+// function to change controls
+async function changeControls(type) {
+  switch (type) {
+    case 'requestScreenShare': {
+      // hide start btn
+      startBtn.hidden = true;
+      startBtn.disabled = false;
+
+      // show the start pip btn
+      startPipBtn.hidden = false;
+
+      // show the video
+      video.hidden = false;
+
+      // hide the cancel pip btn
+      cancelPipBtn.hidden = true;
+      break;
+    }
+
+    case 'startPip': {
+      // hide the start pip btn
+      startPipBtn.hidden = true;
+      startPipBtn.disabled = false;
+
+      // hide the video
+      video.hidden = true;
+
+      // show the cancel pip btn
+      cancelPipBtn.hidden = false;
+
+      break;
+    }
+
+    case 'cancelPip': {
+      // hide the cancel pip btn
+      cancelPipBtn.hidden = true;
+      cancelPipBtn.disabled = false;
+
+      // show the start btn
+      startBtn.hidden = false;
+
+      // hide others
+      video.hidden = true;
+      startPipBtn.hidden = true;
+
+      break;
+    }
+  }
+}
 // function to handle the request for screen share
 async function requestScreenShare() {
   try {
     startBtn.disabled = true;
-
     const mediaCaptureStream = await navigator.mediaDevices.getDisplayMedia();
     if (mediaCaptureStream) {
       video.srcObject = mediaCaptureStream;
-      video.hidden = false;
-      if (!startBtn.hidden) {
-        startBtn.hidden = true;
-        startPipBtn.hidden = false;
-      }
-      startBtn.disabled = false;
+      await changeControls('requestScreenShare');
     }
   } catch (err) {
     console.error('Error:', err);
+    startBtn.disabled = true;
   }
 }
 
@@ -29,32 +73,25 @@ async function requestScreenShare() {
 async function startPipMode() {
   try {
     const result = await video.requestPictureInPicture();
-    startPipBtn.disbaled = true;
+    startPipBtn.disabled = true;
     if (result) {
-      video.hidden = true;
-      startPipBtn.hidden = true;
-      cancelPipBtn.hidden = false;
-      startPipBtn.disbaled = false;
+      await changeControls('startPip');
     }
   } catch (err) {
     console.error('Error starting PIP Mode', err);
+    startPipBtn.disabled = true;
   }
 }
 
 // function to cancel Picture in Picture Mode
-
 async function cancelPipMode() {
   try {
     await document.exitPictureInPicture();
     cancelPipBtn.disbaled = true;
-
-    video.hidden = true;
-    startBtn.hidden = false;
-    startPipBtn.hidden = true;
-    cancelPipBtn.hidden = true;
-    cancelPipBtn.disbaled = false;
+    await changeControls('cancelPip');
   } catch (err) {
     console.error('Error cancelling PIP Mode', err);
+    cancelPipBtn.disbaled = true;
   }
 }
 
@@ -63,10 +100,5 @@ startPipBtn.addEventListener('click', startPipMode);
 cancelPipBtn.addEventListener('click', cancelPipMode);
 
 video.addEventListener('leavepictureinpicture', () => {
-  cancelPipBtn.disbaled = true;
-  video.hidden = true;
-  startBtn.hidden = false;
-  startPipBtn.hidden = true;
-  cancelPipBtn.hidden = true;
-  cancelPipBtn.disbaled = false;
+  changeControls('cancelPip');
 });
