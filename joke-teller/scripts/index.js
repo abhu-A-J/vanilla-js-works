5edbaeb07fa3c0d9151e1c370ea9be8097415910
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-16 14:59:58 +0530

    Initial setup for markup and styles

---
+++ joke-teller/scripts/index.js
@@ -0,0 +1,5 @@
+console.log('Script connected successfully!');
+
+//https://v2.jokeapi.dev/joke/Dark?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart
+
+//http://api.voicerss.org/?key=1234567890QWERTY&hl=en-us&src=Hello, world!
