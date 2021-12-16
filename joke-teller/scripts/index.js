9abfde0b9b1c518fa646ebbbb3dc860adb075f2b
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-16 15:15:51 +0530

    Completion of the joke teller using voices rss

--- joke-teller/scripts/index.js
+++ joke-teller/scripts/index.js
@@ -1,5 +1,57 @@
-console.log('Script connected successfully!');
+// Get the DOM Elements
+const button = document.querySelector('button');
+const audioElem = document.querySelector('#audio');
 
-//https://v2.jokeapi.dev/joke/Dark?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart
+const VOICES_RSS_KEY = 'API KEY';
 
-//http://api.voicerss.org/?key=1234567890QWERTY&hl=en-us&src=Hello, world!
+// function to set disbaled state for button
+function shouldDisableBtn(state) {
+  button.disabled = state;
+}
+
+// function that use voices rss to perform text to speech conversion
+async function tellJoke(joke) {
+  const URL = `http://api.voicerss.org/?key=${VOICES_RSS_KEY}&hl=en-us&src=${joke}`;
+  try {
+    VoiceRSS.speech({
+      key: VOICES_RSS_KEY,
+      src: joke,
+      hl: 'en-us',
+      v: 'Linda',
+      r: 0,
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false,
+    });
+
+    audioElem.addEventListener('ended', () => {
+      shouldDisableBtn(false);
+    });
+  } catch (err) {
+    shouldDisableBtn(false);
+    console.error('Error fetching joke', err);
+  }
+}
+
+// function to fetch a joke from API
+async function getJoke() {
+  const URL =
+    'https://v2.jokeapi.dev/joke/Programming,Dark?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart';
+
+  shouldDisableBtn(true);
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    let jokeToHear = '';
+    if (data.setup && data.delivery) {
+      jokeToHear = `${data.setup}...${data.delivery}`;
+      await tellJoke(jokeToHear);
+    }
+  } catch (err) {
+    shouldDisableBtn(false);
+    console.error('Error fetching joke', err);
+  }
+}
+
+button.addEventListener('click', getJoke);
