06dd2d34618c65daf3deefc451d85a87c3e3dabc
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-17 12:51:59 +0530

    Add slideIn and slideOut animations on overlay

--- animated-navbar/scripts/index.js
+++ animated-navbar/scripts/index.js
@@ -1,8 +1,16 @@
 const menuBar = document.querySelector('#menu-bar');
+const navbarOverlay = document.querySelector('#navbar-overlay');
 
 /* Function to toggle the menu bar */
 async function toggleMenu() {
   menuBar.classList.toggle('change');
+  if (menuBar.classList.contains('change')) {
+    navbarOverlay.classList.add('slideIn');
+    navbarOverlay.classList.remove('slideOut');
+  } else {
+    navbarOverlay.classList.add('slideOut');
+    navbarOverlay.classList.remove('slideIn');
+  }
 }
 
 /* Listen to click event on menu bar */
