c0003b00d5527faaeaae1fe891704c177bc0d8a8
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-17 11:20:17 +0530

    Add JS to toggle theme by setting the data-theme attribute on root element

--- light-dark-mode/scripts/index.js
+++ light-dark-mode/scripts/index.js
@@ -1,1 +1,25 @@
-console.log('Script connected successfully!');
+const themeToggle = document.querySelector('#theme-toggle');
+
+/* Helper function to set theme attribue on root element */
+async function setThemeAttribute(theme = 'light') {
+  localStorage.setItem('theme', theme);
+  document.documentElement.setAttribute('data-theme', theme);
+  themeToggle.checked = theme === 'dark';
+}
+
+/* Function to set the theme */
+async function toggleTheme(e) {
+  const { checked } = e.target;
+
+  if (checked) {
+    setThemeAttribute('dark');
+  } else {
+    setThemeAttribute('light');
+  }
+}
+
+/* Listen to the change event of the checkbox */
+themeToggle.addEventListener('change', toggleTheme);
+
+const currentTheme = localStorage.getItem('theme') || 'light';
+setThemeAttribute(currentTheme);
