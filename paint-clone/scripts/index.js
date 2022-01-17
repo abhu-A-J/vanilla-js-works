64d1c705e207e8f99be12ebee5d04607a2ca503a
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2022-01-17 15:00:41 +0530

    Setup DOM elements

--- paint-clone/scripts/index.js
+++ paint-clone/scripts/index.js
@@ -1,1 +1,26 @@
-console.log('Script connected successfully!');
+const activeToolEl = document.getElementById('active-tool');
+
+/* Brush Elements */
+const brushColorBtn = document.getElementById('brush-color');
+const brushIcon = document.getElementById('brush');
+const brushSize = document.getElementById('brush-size');
+const brushSlider = document.getElementById('brush-slider');
+
+/* Bucket */
+const bucketColorBtn = document.getElementById('bucket-color');
+
+/* Eraser Elemenet */
+const eraser = document.getElementById('eraser');
+
+/* Clear Canvas */
+const clearCanvasBtn = document.getElementById('clear-canvas');
+
+/* Storage */
+const saveStorageBtn = document.getElementById('save-storage');
+const loadStorageBtn = document.getElementById('load-storage');
+const clearStorageBtn = document.getElementById('clear-storage');
+
+/* Download */
+const downloadBtn = document.getElementById('download');
+
+const { body } = document;
